54a101f4dba9e7fa37fd6e5743c1cd4e16d594e7
aminul124387 <aminul124387@gmail.com>
2025-11-18 17:30:17 +0600

    dashboard function pdated

--- models/dashboard.py
+++ models/dashboard.py
@@ -14,13 +14,148 @@ class Dashboard(models.Model):
 
 
     def custom_dashboard(self, cr, uid, start_date=None, end_date=None, context=None):
-        result={'opd':100,'bill':500,'other':450,'sting':'strings value'}
-        return result  
+        st_dat = (start_date or date.today()).strftime('%Y-%m-%d 00:00:00')
+        end_dat = (end_date or date.today()).strftime('%Y-%m-%d 23:59:59')
+
+        result = {
+        }
+
+        opd_q = """
+            SELECT 
+                COUNT(ot.id) AS ticket_count, 
+                SUM(otl.total_amount) AS total_amount_sum 
+            FROM opd_ticket ot 
+            JOIN opd_ticket_line otl ON otl.opd_ticket_id = ot.id 
+            WHERE ot.state = 'confirmed'
+            AND ot.create_date >= '%s' 
+            AND ot.create_date <= '%s';
+        """
+
+        cr.execute(opd_q % (st_dat, end_dat))
+        data = cr.fetchone()   # fetch single row (count, sum)
+
+        ticket_count = int(data[0] or 0)
+        total_amount = int(data[1] or 0)
+
+        result['opd_income'] = {
+            'count': ticket_count,
+            'amount': total_amount
+        }
+
+# this block is for dental
+
+        opd_dental_q = """
+        SELECT 
+        COUNT(ot.id) AS dental_opd_count,
+        SUM(otl.total_amount) AS dental_opd_amount
+        FROM opd_ticket ot
+        JOIN opd_ticket_line otl ON otl.opd_ticket_id = ot.id
+        WHERE ot.state = 'confirmed'
+        AND ot.create_date >= '%s'
+        AND ot.create_date <= '%s'
+        AND otl.department ILIKE '%%dental%%';
+        """
+
+        cr.execute(opd_dental_q % (st_dat, end_dat))
+        opd_dental_data = cr.fetchone()
+
+        opd_dent_count = int(opd_dental_data[0] or 0)
+        opd_dent_amount = int(opd_dental_data[1] or 0)
+
+        result['dental_opd'] = {
+            'count': opd_dent_count,
+            'amount': opd_dent_amount
+        }
+
+
+        dental_bill_q = """
+            SELECT 
+                COUNT(br.id) AS bill_count,
+                SUM(br.grand_total) AS total_bill_amount
+            FROM bill_register br
+            JOIN bill_register_line brl ON brl.bill_register_id = br.id
+            WHERE br.state = 'confirmed'
+            AND br.create_date >= '%s'
+            AND br.create_date <= '%s'
+            AND brl.department ILIKE '%%dental%%';
+        """
+
+        cr.execute(dental_bill_q % (st_dat, end_dat))
+        bill_data = cr.fetchone()
+
+        bill_count = int(bill_data[0] or 0)
+        bill_amount = int(bill_data[1] or 0)
+
+        result['dental_income'] = {
+            'count': bill_count,
+            'amount': bill_amount
+        }
+
+    # dental blcok end
+
+
+    # physiotherapy block 
+    
+        opd_physio_q = """
+        SELECT 
+        COUNT(ot.id) AS dental_opd_count,
+        SUM(otl.total_amount) AS dental_opd_amount
+        FROM opd_ticket ot
+        JOIN opd_ticket_line otl ON otl.opd_ticket_id = ot.id
+        WHERE ot.state = 'confirmed'
+        AND ot.create_date >= '%s'
+        AND ot.create_date <= '%s'
+        AND otl.department ILIKE '%%physiot%%';
+        """
+
+        cr.execute(opd_physio_q % (st_dat, end_dat))
+        opd_physio_data = cr.fetchone()
+
+        opd_physio_count = int(opd_physio_data[0] or 0)
+        opd_physio_amount = int(opd_physio_data[1] or 0)
+
+        result['physiotherpay_opd'] = {
+            'count': opd_dent_count,
+            'amount': opd_dent_amount
+        }
+
+
+        physio_bill_q = """
+            SELECT 
+                COUNT(br.id) AS bill_count,
+                SUM(br.grand_total) AS total_bill_amount
+            FROM bill_register br
+            JOIN bill_register_line brl ON brl.bill_register_id = br.id
+            WHERE br.state = 'confirmed'
+            AND br.create_date >= '%s'
+            AND br.create_date <= '%s'
+            AND brl.department ILIKE '%%physiot%%';
+        """
+
+        cr.execute(physio_bill_q % (st_dat, end_dat))
+        physio_data = cr.fetchone()
+
+        physiotherapy_bill_count = int(physio_data[0] or 0)
+        physiotherapy_bill_amount = int(physio_data[1] or 0)
+
+        result['physiotherapy_bill'] = {
+            'count': bill_count,
+            'amount': bill_amount
+        }
+
+    # end physiotherpay 
+
+        return result
+
+    
+    # def cash_collection(self,cr,uid,context=None):
 
     def _compute_field_list(self):
         dashboard = self.env['dashboard.settings'].search([], limit=1, order='id desc')
         lists = dashboard.line_ids
         last_slices_list = []
+
+        # self.custom_dashboard()
 
         # Determine dashboard date range
         if dashboard.date_mode == 'yesterday':
